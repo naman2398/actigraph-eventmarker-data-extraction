1566dea3c2faf5dcaaad8bc16961d4eee7596011
naman2398 <naman230598@gmail.com>
2026-01-13 21:50:08 +0530

    Include virtual environment in deployment

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/site/wwwroot/src
+cd /home/site/wwwroot
+source antenv/bin/activate
+cd src
 python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
